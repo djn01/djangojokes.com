d9cdbd815c228bc7c35979ac10c5f69d1ec10aaa
djn01e <dnabmedia@gmail.com>
2023-08-08 19:53:11 +0200

    jokes list

--- jokes/views.py
+++ jokes/views.py
@@ -1,9 +1,21 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView,  DetailView, ListView, UpdateView
 
 from .models import Joke
 
+# Joke Create View
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+# Joke Detail View
 class JokeDetailView(DetailView):
     model = Joke
 
+# Joke List View
 class JokeListView(ListView):
     model = Joke
+
+# Joke Update View
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
