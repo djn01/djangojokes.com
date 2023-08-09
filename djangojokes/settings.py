9dc1946b5070647d71d4352b046e9e429f0075fb
djn01e <dnabmedia@gmail.com>
2023-08-09 15:21:17 +0200

    jokes

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': 'Brumb~123',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
